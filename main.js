729120ffed03a1474da50f052728e2e31560375b
Altynbekow <152465429+Altynbekow@users.noreply.github.com>
2023-12-02 11:53:30 +0600

    lesson 2

---
+++ main.js
@@ -0,0 +1,43 @@
+// типы данных
+// 1string
+// 
+// ключивые слова var let const 
+// операторы (=,==,===,+,-,/.*,>< >= <=,)
+var carname = "fit"
+var username=`kolbai`
+// 2 number
+var appleprice2=100
+var battterymass=70
+
+// 3 boolean
+var issnow=false
+var issnow2=true
+// 4 null
+var baha=null
+baha=`baha`
+// 5 undefined
+var jok;
+// 6--
+var id=Symbol('userid')
+// 7-- bightint
+var maxkm =100n
+
+// 8 object{},array[]
+var headermenu=['home','about','contact',]
+var table={
+    // key:value
+    color:'red',
+    height:2,
+    width:3,
+    matterial:'metal'
+}
+var user={
+    name:'kolbai',
+    ismarried:false,
+    salary:500000,
+    age:20,
+    car:null,
+}
+
+
+
